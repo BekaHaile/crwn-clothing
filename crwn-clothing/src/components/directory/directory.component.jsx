ee533c09b2b613891d358a3a85c518b6165d3a64
Beka Haile <BHaile@excellerentsolutions.com>
2021-08-25 15:54:30 +0300

    Updated our menu-items to navigate withRouter HOC

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -48,7 +48,7 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, id, imageUrl, size}) => (<MenuItem id={id} title={title} imageUrl={imageUrl} size={size}/>))
+                    this.state.sections.map(({...otherSectionProps}) => (<MenuItem {...otherSectionProps} />))
                 }
             </div>
             
