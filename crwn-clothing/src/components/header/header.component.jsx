eb4afbaedfe63c673ccde9cd8e48a69eb9271b96
Beka Haile <BHaile@excellerentsolutions.com>
2021-09-06 17:53:14 +0300

    Revert "Revert "Integrated app with firebase and added sign in with sign in with google option and and sign up components.""
    This reverts commit a69a6dc233a9e71011f5ef900d0130f0724e3259.

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
 import "./header.styles.scss";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
+  console.log(currentUser);
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -18,6 +20,15 @@ const Header = () => {
         <Link to="/shop" className="option">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link to="/signin" className="option">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
