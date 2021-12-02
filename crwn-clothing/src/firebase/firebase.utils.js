acb75b8febcde7d15a800379a083c7ac6d61c00c
Beka Haile <BHaile@excellerentsolutions.com>
2021-12-02 15:24:05 +0300

    Firebase edit

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -13,6 +13,26 @@ const config = {
   measurementId: "G-2YVPC0SQFS",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const userSnap = await userRef.get();
+
+  if (!userSnap.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("Error creating user: " + error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
