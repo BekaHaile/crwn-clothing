e88e74f8e88f31e0808aa8ab30ff48d50dc74720
Beka Haile <barbeka.tola@gmail.com>
2022-11-09 20:37:42 -0500

    React class components updated to functional components and firebase util updated

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import "./App.css";
+import { UserContext } from "./contexts/user.context";
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Authentication from "./pages/authentication/authentication.component";
-import Header from "./components/header/header.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
-class App extends React.Component {
-  constructor() {
-    super();
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
-    this.state = {
-      currentUser: null,
-    };
-  }
-
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
-
-      this.setState({ currentUser: user });
-
-      console.log(user);
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/auth" component={Authentication} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { currentUser } = useContext(UserContext);
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route
+          path="auth"
+          element={
+            currentUser ? <Navigate to="/" replace /> : <Authentication />
+          }
+        />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
 
-ReactDOM.render(
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+
+import "./index.scss";
+
+const rootElement = document.getElementById("root");
+
+render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
