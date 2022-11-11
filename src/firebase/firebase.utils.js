89a136f6041451a0da43e249f2d94ebaa11431eb
Beka Haile <barbeka.tola@gmail.com>
2022-11-12 01:26:16 +0400

    Auth listener added

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,54 +1,69 @@
-import firebase from "firebase/compat/app";
-
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
-
+import { initializeApp } from 'firebase/app';
 import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-} from "firebase/auth";
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
-const config = {
-  apiKey: "AIzaSyA1EuyrP3hqHu9EjIqd5p9SC8IpYLSOd7Q",
-  authDomain: "crwn-db-52f08.firebaseapp.com",
-  projectId: "crwn-db-52f08",
-  storageBucket: "crwn-db-52f08.appspot.com",
-  messagingSenderId: "400235070326",
-  appId: "1:400235070326:web:dfca7805f50d9634c53057",
-  measurementId: "G-2YVPC0SQFS",
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userDocRef = doc(db, 'users', userAuth.uid);
 
-  const userSnap = await userRef.get();
+  const userSnapshot = await getDoc(userDocRef);
 
-  if (!userSnap.exists) {
+  if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
+
     try {
-      await userRef.set({ displayName, email, createdAt, ...additionalData });
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
     } catch (error) {
-      console.log("Error creating user: " + error.message);
+      console.log('error creating the user', error.message);
     }
   }
 
-  return userRef;
+  return userDocRef;
 };
-
-firebase.initializeApp(config);
-
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-export default firebase;
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
@@ -61,3 +76,8 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
