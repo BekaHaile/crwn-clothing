8d500d80cf780295c9dc856d4745d11c9737c559
Beka Haile <barbeka.tola@gmail.com>
2022-11-18 23:57:38 +0400

    Getting categories data from firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,4 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
   setProducrts: () => null,
@@ -8,6 +10,16 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const value = { products, setProducts };
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+
+      console.log(categoryMap);
+    };
+
+    getCategoriesMap();
+  }, []);
 
   return (
     <ProductsContext.Provider value={value}>
