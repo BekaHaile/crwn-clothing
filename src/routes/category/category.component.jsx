bec7c3fffaa1d8d01068561f8a2e04c651cae610
Beka Haile <barbeka.tola@gmail.com>
2022-11-20 23:54:33 +0400

    Category container styling conflict fixed

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,7 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
+
 import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
+
 import { CategoriesContext } from "../../contexts/categories.context";
+
+import './category.styles.scss'
 
 const Category = () => {
   const { category } = useParams();
@@ -13,7 +17,7 @@ const Category = () => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
   return (
-    <div className="category-shop-container">
+    <div className="category-container">
       {products &&
         products.map((product) => (
           <ProductCard key={product.id} product={product} />
