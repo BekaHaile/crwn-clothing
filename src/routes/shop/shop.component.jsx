e064af4cdd35dd8975bf387670c2218c3aaece3f
Beka Haile <barbeka.tola@gmail.com>
2022-11-21 19:14:33 +0400

    Category and shop style changes to styled component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import CatgegoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 
-import "./shop.styles.scss";
-
 const Shop = () => {
   return (
     <Routes>
