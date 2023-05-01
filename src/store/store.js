ce51f88924b52e0605e5d3af8853330a8c4b3bbe
Beka Haile <barbeka.tola@gmail.com>
2023-05-01 11:54:14 +0400

    Custom Middleware logger added

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 // root-reducer
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("nextState: ", store.getState());
+};
+
+const middlewares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
